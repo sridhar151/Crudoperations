01d04cc6de569c9e0defab242534e5033570126b
sridhar151 <sridhargeekuru00@gmail.com>
2019-12-27 12:05:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+angular.module('App', []).controller('CrudCtrl', ['$scope',
+	function($scope) {
+	  $scope.Profiles = [
+	      {
+	        name : "srinivas",
+	        country : "indonesia",
+	        editable : false
+	      },
+	      {
+	        name : "anil kumar",
+	        country : "thailand",
+	        editable : false
+	      }
+	    ];
+	 
+	 $scope.entity = {}
+	    
+	 $scope.edit = function(index){
+	   $scope.entity = $scope.Profiles[index];
+	   $scope.entity.index = index;
+	   $scope.entity.editable = true;
+	 }
+	    
+	 $scope.delete = function(index){
+	   $scope.Profiles.splice(index,1);
+	 }
+	    
+	 $scope.save = function(index){
+	   $scope.Profiles[index].editable = false;
+	   
+	 }
+	    
+	 $scope.add = function(){
+	   $scope.Profiles.push({
+	      name : "",
+        country : "",
+        editable : true
+	   })
+	 }
+	}
+]);
